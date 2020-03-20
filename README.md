d360d773a52900693818a6d1028d000a25ceff5c
李连成 <liancheng.li@yunhutech.com>
2020-03-20 15:33:50 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# todoList-by-Node git init git add README.md git commit -m first commit git remote add origin git@github.com:HARRISKING/todoList-by-Node.git git push -u origin master
---
+++ todo.js
@@ -0,0 +1,64 @@
+const fs = require('fs');
+const path = require('path');
+
+const filePath = path.join(__dirname, 'db')
+const command = process.argv[2]
+const content = process.argv[3]
+const index = Number(process.argv[4] - 1)
+
+let list = []
+
+initDb()
+
+switch (command) {
+    case 'add':
+        list.push({
+            content: content,
+            isComplete: false
+        })
+        break
+    case 'ls':
+        JSON.parse(fs.readFileSync(filePath, 'utf-8')).map(item => console.log(
+            `${item.isComplete ? '[o]' : '[x]'} ${item.content}`
+        ))
+        break
+    case 'edit':
+        try {
+            list[index]['content'] = content
+        } catch (error) {
+            console.log('你输入的命令有误')
+        }
+        break
+    case 'ok':
+        list[content - 1].isComplete = true
+        break
+    case 'delete':
+        list.splice(content - 1, 1)
+        break
+    case 'clear':
+        console.log('clear', filePath)
+        fs.unlinkSync(filePath)
+        break
+    default:
+        console.log('你输入的命令是:' + command + '我不懂你要做什么')
+        break
+}
+
+if (command === 'add' && 'edit' && 'ok' && 'delete') {
+    write(filePath, JSON.stringify(list))
+}
+
+// help function
+function initDb() {
+    if (fs.existsSync(filePath)) {
+        list = JSON.parse(fs.readFileSync(filePath, 'utf-8'))
+        console.log('list init', list)
+    } else {
+        write(filePath, '')
+        console.log('不存在', list)
+    }
+}
+
+function write(filePath, content) {
+    fs.writeFileSync(filePath, content)
+}
